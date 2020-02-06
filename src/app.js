635ceb4eda7958c8fdbe9308d01d355740844ee9
kuo-hung <minhsin3208@gmail.com>
2020-02-06 21:03:48 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,16 @@
+window.alertify = require('alertifyjs');
+require('./bootstrap')
+
+import StudentNameList from './components/StudentNameList'
+import AddStudent from './components/AddStudent'
+
+new Vue({
+    el: '#app',
+    components: { StudentNameList, AddStudent },
+    data() {
+        return {
+            title: 'Student List',
+        }
+    },
+    methods: {}
+})
